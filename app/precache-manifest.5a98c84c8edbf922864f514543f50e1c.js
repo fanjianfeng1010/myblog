3402ecbcdbb58da97bbb83dfb6006a2e92375b39
fanjianfeng1010 <264589826@qq.com>
2019-10-29 19:59:57 +0800

    update

---
+++ app/precache-manifest.5a98c84c8edbf922864f514543f50e1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07f5395211b54876d7d9797183aeaf54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c325535aebec57f515d5",
+    "url": "/static/css/2.6547dd70.chunk.css"
+  },
+  {
+    "revision": "12c6bbd490b8bd9f45ab",
+    "url": "/static/css/main.043db62d.chunk.css"
+  },
+  {
+    "revision": "c325535aebec57f515d5",
+    "url": "/static/js/2.04884012.chunk.js"
+  },
+  {
+    "revision": "12c6bbd490b8bd9f45ab",
+    "url": "/static/js/main.ade8055e.chunk.js"
+  },
+  {
+    "revision": "531a2b9fed4c75b9f3b6",
+    "url": "/static/js/runtime-main.0dbf599d.js"
+  }
+]);
